00980f0f54ede4be3c01ccbeb12b246e9e9ad6d1
Adam Myers <myers.adam.k@gmail.com>
2020-11-22 12:45:03 -0500

    Initial commit

---
+++ src/main/java/com/digitalcocoa/mtg/deck/organizer/MtgDeckOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcocoa.mtg.deck.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgDeckOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgDeckOrganizerApplication.class, args);
+	}
+
+}
